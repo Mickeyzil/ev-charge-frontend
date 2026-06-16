d39b17d654cf75d0f8c6d5fa486434985d2a8739
Mickey Zilberman <mikizil04@gmail.com>
2026-06-16 17:42:31 +0300

    feat: implement login form validation with regex and trim

---
+++ JS/Login.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded',()=> {
+    const loginForm = document.getElementById('login-form');
+    loginForm.addEventListener('submit', (event) =>{
+        event.preventDefault();
+
+        const errorBox = document.getElementById('error-message');
+        const user_email = document.getElementById('email').value.trim();
+        const user_password = document.getElementById('password').value.trim();
+        
+        errorBox.classList.add('hidden');
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+        if(user_email == "" || user_password == "")
+        {
+            errorBox.classList.remove('hidden');
+            errorBox.innerHTML = "Please fill all fields";
+        }
+        else if(!emailPattern.test(user_email))
+        {
+            errorBox.classList.remove('hidden');
+            errorBox.innerHTML = "Please enter valid email";
+        }
+        else if(!passwordPattern.test(user_password))
+        {
+            errorBox.classList.remove('hidden');
+            errorBox.innerHTML = "Please enter valid password";
+        }
+        else{
+            location.href="MainMenu.html";
+        }
+    });
+})
